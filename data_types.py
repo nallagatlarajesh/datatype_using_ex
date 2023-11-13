be92ddcf2b0577803563e78c7842f0f4a554fb10
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-11-13 11:12:52 +0530

    uses
    exapmle

---
+++ data_types.py
@@ -0,0 +1,8 @@
+list=["students of a class "]
+tuple=("months in a year")
+sets={"artefacts in amuseum"}
+dict={"if or data is being constantly modified or we need a faster lookup based onn key or we need a logical association between the key :value pair ,it is adb=vised to use dictionary"}
+list
+tuple
+sets
+dict
